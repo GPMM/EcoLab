da501661397b4af336ed15ca05b010312c138ad4
Lewiks <luik98@gmail.com>
2020-09-15 18:13:28 -0300

    Created Menu Manager

---
+++ Assets/Scripts/EcoClean/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace EcoClean
+{
+    public class MenuManager : MonoBehaviour
+    {
+        #region Serialized variables
+        [SerializeField]
+        private GameObject startingMenu;
+        #endregion
+
+        #region Local variables
+        private List<GameObject> menus = new List<GameObject>();
+        private GameObject activeMenu = null;
+        #endregion Properties
+
+        #region Methods
+        private void Awake()
+        {
+            activeMenu = startingMenu;
+
+            // Adds every panel under this MenuContainer as a Menu.
+            foreach (Transform child in transform)
+            {
+                GameObject menu = child.gameObject;
+
+                menus.Add(menu);
+
+                // Sets the menu as active only if it is the starting menu, otherwise sets it to inactive.
+                menu.SetActive(menu == startingMenu);
+            }
+        }
+
+        public void UIMoveTo(GameObject targetMenu)
+        {
+            if (menus.Exists(x => x.gameObject == targetMenu))
+            {
+                activeMenu.SetActive(false);
+
+                targetMenu.SetActive(true);
+
+                activeMenu = targetMenu;
+            }
+        }
+        #endregion Methods
+    }
+}
